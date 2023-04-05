0b7c036f8b2b6e5fa2cbba9bc5a971d18e6887d0
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-05 09:52:38 +0430

    searchBox Added , Project Done

--- src/Components/TransActionsComponent.jsx
+++ src/Components/TransActionsComponent.jsx
@@ -1,20 +1,48 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const TransActionsComponent = ({ transactions }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredTnx, setFilteredTnx] = useState(transactions);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilteredTnx(transactions);
+      return;
+    }
+    const filtered = transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTnx(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transactions]);
+
+  if(!transactions.length) return <p style={{textAlign: 'center' , fontSize: '13px' , margin:"10px 0"}}>Add Transactions</p>
   return (
     <section className="TransActionsComponent">
-      {transactions.length &&
-        transactions.map((t) => (
-          <div
-            key={t.id}
-            className="transactions"
-            style={
-              t.type === "expense" ? { color: "red" } : { color: "green" }
-            }>
-            <span>{t.desc}</span>
-            <span>
-              {t.type === "income" ? "+" : "-"} $ {t.amount}
-            </span>
-          </div>
-        ))}
+      <input type="text" value={searchItem} onChange={changeHandler} placeholder="Search For Tnx" className="search"/>
+      {filteredTnx.length
+        ? filteredTnx.map((t) => (
+            <div
+              key={t.id}
+              className="transactions"
+              style={
+                t.type === "expense" ? { color: "red" } : { color: "green" }
+              }>
+              <span>{t.desc}</span>
+              <span>
+                {t.type === "income" ? "+" : "-"} $ {t.amount}
+              </span>
+            </div>
+          ))
+        : <p style={{textAlign: 'center' , fontSize: '13px'}}>No Item Matchs !</p>}
     </section>
   );
 };
